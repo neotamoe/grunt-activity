7d0bf7762d1126492f4ec6b06ba7530e17d24589
Millie Walsh <millie@primeacademy.io>
2017-05-14 19:24:16 -0500

    a version of the client script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,20 @@
-console.log('sourced');
+var myApp = angular.module('myApp', []);
+
+myApp.controller('HeroController', function() {
+
+  // grab the view model
+  var vm = this;
+
+  // function to add hero
+  vm.addHero = function() {
+
+  }
+
+  // function to get heros
+  vm.getHero = function() {
+
+  }
+
+  // when controller loads it will get all heros
+  vm.getHero();
+});
